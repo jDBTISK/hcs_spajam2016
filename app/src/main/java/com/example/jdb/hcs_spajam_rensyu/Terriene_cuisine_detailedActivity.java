42c49725bc91ea1bbf116f813d810ae5deff52b9
近藤泰介 <jDB@jDB-MacBook-Pro.local>
2016-05-21 20:11:55 +0900

    おフランス 作りました

---
+++ app/src/main/java/com/example/jdb/hcs_spajam_rensyu/Terriene_cuisine_detailedActivity.java
@@ -0,0 +1,13 @@
+package com.example.jdb.hcs_spajam_rensyu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Terriene_cuisine_detailedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terriene_cuisine_detailed);
+    }
+}
